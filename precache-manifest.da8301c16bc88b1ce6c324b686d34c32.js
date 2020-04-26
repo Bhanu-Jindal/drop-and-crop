85ae6b41505ed55ba41e7458ff1ceba4aaafee54
Bhanu-Jindal <bhanurajjindal@gmail.com>
2020-04-27 01:43:47 +0530

    Updates

---
+++ precache-manifest.da8301c16bc88b1ce6c324b686d34c32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e28d44e2f1e6597d59bc1c303c1dd2",
+    "url": "/drop-and-crop/index.html"
+  },
+  {
+    "revision": "7becb2cd991de50c5c77",
+    "url": "/drop-and-crop/static/css/2.cbeaa07e.chunk.css"
+  },
+  {
+    "revision": "3ad12c34b61b23a448c6",
+    "url": "/drop-and-crop/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7becb2cd991de50c5c77",
+    "url": "/drop-and-crop/static/js/2.e67fe5cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/drop-and-crop/static/js/2.e67fe5cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad12c34b61b23a448c6",
+    "url": "/drop-and-crop/static/js/main.57c5ee05.chunk.js"
+  },
+  {
+    "revision": "33e1ff44b2439815186b",
+    "url": "/drop-and-crop/static/js/runtime-main.685e1e7d.js"
+  }
+]);
